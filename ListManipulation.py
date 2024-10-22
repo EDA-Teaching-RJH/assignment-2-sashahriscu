56a5311e7d1f6388d04e432890b9e6dc20e2a0cf
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-22 02:03:52 +0000

    completion

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,8 +1,12 @@
 def main():
-    numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
-    numbers.sort()
-    numbers = [num for num in numbers if num != 1]
-    numbers.extend([7, 8])
-    print(numbers)
+    values = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+    values.sort()
+    
+    while 1 in values:
+        values.remove(1)
+    
+    values.extend([7, 8])
+    
+    print(values)
 
 main()
