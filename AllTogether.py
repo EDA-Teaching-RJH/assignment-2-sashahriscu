0561cff26e0ff8fe406732e0218cda1d0f53081f
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-25 12:36:23 +0100

    Update AllTogether.py

--- AllTogether.py
+++ AllTogether.py
@@ -7,7 +7,7 @@ def main():
 
     index = 0
     while index < len(random_numbers):
-# modulo of % 2 to distinguish even numbers
+# modulo %2 to distinguish even numbers
         if random_numbers[index]%2 == 0:
             random_numbers.pop(index)
 # .pop removes the item at the specified index
