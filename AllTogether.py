2a85290601c80f056bdbb3d1ec9ce7ac0914b51d
Alexandra Hriscu <ah2148@kent.ac.uk>
2024-10-25 11:35:48 +0000

    completion

--- AllTogether.py
+++ AllTogether.py
@@ -3,17 +3,17 @@ import random
 def main():
 # generating range of numbers
     random_numbers = [random.randint(1, 100) for _ in range(10)]
-    print("List of numbers:", random_numbers)
+    print("Numbers list :", random_numbers)
 
     index = 0
     while index < len(random_numbers):
 # modulo of % 2 to distinguish even numbers
-        if random_numbers[index] % 2 == 0:
+        if random_numbers[index]%2 == 0:
             random_numbers.pop(index)
 # .pop removes the item at the specified index
         else:
             index += 1
-    print("After removing even numbers:", random_numbers)
+    print("After removing even numbers :", random_numbers)
 # return result of removing even numbers from the list
 
 # call principal function
